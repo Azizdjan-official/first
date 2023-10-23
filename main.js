511033c798592f067c493a370ebd1b88394a5c28
azizdjan_official <tulaganovazizjon@gmail.com>
2023-10-24 01:00:05 +0500

    uyishi qilindi

---
+++ main.js
@@ -0,0 +1,15 @@
+let first_name = prompt('what is your name ?')
+let last_name = prompt('what is your surname ?')
+let age = prompt('how old are you brother ?')
+let study = prompt('where do you study ?')
+let color= prompt('what is your favourite color ?')
+let goal =prompt('what is your goal in this life ?')
+let car =prompt('what is your dream car ?')
+let hobby =prompt('what is your hobby ?')
+let language =prompt('what is your national language ?')
+let country =prompt('what is your country  ?')
+
+console.log( '  My name is ' + first_name + '\n ' + ' My surname is ' + last_name + '\n ' + ' I am ' + age
+ + ' old ' + '\n ' + ' I study at ' + study + '\n ' + ' Favourite color is ' + color + '\n ' + ' My goal in this lify is ' + goal
+ + '\n ' + ' My dream car is ' + car + '\n ' + ' My hobby is ' + hobby + '\n ' + ' My national language is ' +
+ language + '\n ' + ' I am from ' + country );
